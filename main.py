c1ace5424143b828d59057d3ba1dae438089ddf7
snow10100 <169849961+snow10100@users.noreply.github.com>
2024-08-25 01:12:39 +0300

    switch ui to use pentest_graph

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from fastapi import FastAPI
+import json
+from typing_extensions import Annotated, TypedDict
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
 from rich import print
 import os
 from langgraph.prebuilt import create_react_agent
 from langchain_core.tools import tool
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, ToolMessage
 from langchain_community.tools import ShellTool, HumanInputRun
 from langchain_experimental.tools import PythonREPLTool
-from api.routes import router
+# from api.routes import router
 
 # from langchain_mistralai import ChatMistralAI
 from langchain_anthropic import ChatAnthropic
@@ -24,8 +27,8 @@ from langserve import add_routes
 from fastapi.middleware.cors import CORSMiddleware
 
 
-from lg_pentest.pentest_agent.sup_agent import graph
-# from lg_pentest.pentest_agent.agent import pentest_graph
+# from lg_pentest.pentest_agent.sup_agent import graph
+from lg_pentest.pentest_agent.agent import pentest_graph
 
 # Set all CORS enabled origins
 
@@ -87,10 +90,10 @@ add_routes(
 
 add_routes(
     app,
-    prompt | graph,
+    prompt | pentest_graph,
     path="/graph",
 )
-app.include_router(router)
+# app.include_router(router)
 
 # if __name__ == "__main__":
 #     import uvicorn
@@ -108,8 +111,47 @@ inputs = {
 
 # print(inputs)
 # print()
-for i, chunk in enumerate(pentest_graph.stream(inputs, stream_mode='values')):
-    print(i, chunk)
+# for i, chunk in enumerate(pentest_graph.stream(inputs, stream_mode='values')):
+#     print(i, chunk)
 
 # print()
 # print(pentest_graph.invoke(inputs))
+def event_stream(query: str):
+    # Sends an event every second with data: "Message {i}"
+    initial_state = {
+            # "messages": [HumanMessage(content=query)],
+            "messages": [{"type": "human", "content": query}],
+            # "current_step": "supervisor",
+            "model_status": "scanning",
+            "evaluation": "wow",
+            }
+    for chunk in pentest_graph.stream(initial_state, stream_mode="updates"):
+        for node_name, node_results in chunk.items():
+            print(f'{node_name = }')
+            print(node_results)
+            chunk_messages = node_results.get("messages", [])
+            model_status = node_results.get("model_status", [])
+            for message in chunk_messages:
+                if not message['content']:
+                    continue
+                # if isinstance(message, ToolMessage):
+                if message['type'] == 'tool_calls':
+                    event_str = "event: tool_event"
+                else:
+                    event_str = "event: ai_event"
+                # data_str = f"data: {message.content}"
+                # data_str += f"model_status: {model_status}"
+                data_str = 'data: ' + json.dumps(node_results)
+                # print(f"{event_str}\n{data_str}\n\n")
+                yield f"{event_str}\n{data_str}\n\n"
+            
+
+@app.post("/stream")
+async def stream(query: Annotated[str, Body(embed=True)]):
+    return StreamingResponse(event_stream(query), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
